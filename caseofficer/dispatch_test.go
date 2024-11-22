45c92a7c80a71554629e30553174206dd030224b
advanced-go <mark.wilson.go@outlook.com>
2024-11-22 10:34:52 -0600

    Updating

--- caseofficer/dispatch_test.go
+++ caseofficer/dispatch_test.go
@@ -16,8 +16,12 @@ func (d *dispatchT) setup(_ *caseOfficer, _, _ string) {}
 
 func (d *dispatchT) dispatch(agent *caseOfficer, channel, event string) {
 	switch event {
+	case messaging.StartupEvent:
+		fmt.Printf("test: dispatch(%v) -> [count:%v]\n", event, agent.serviceAgents.Count())
 	case messaging.DataChangeEvent:
 		agent.handler.Trace(agent, channel, event, "Broadcast() -> calendar data change event")
+	case messaging.ShutdownEvent:
+		agent.handler.Trace(agent, channel, event, "shutdown")
 	}
 }
 
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/advanced-go/agents
 go 1.21.4
 
 require (
-	github.com/advanced-go/common v0.0.0-20241122154645-2a513be02501
+	github.com/advanced-go/common v0.0.0-20241122162729-b89ca8775a39
 	github.com/advanced-go/log v0.0.0-20241114210224-f5157780c204
-	github.com/advanced-go/resiliency v0.0.0-20241122155203-85712e23f362
+	github.com/advanced-go/resiliency v0.0.0-20241122163231-4b3543681410
 )
 
 require github.com/google/uuid v1.6.0 // indirect
